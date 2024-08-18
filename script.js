d7322364f410fa94fdefd815abfd60ed1c1ab85d
Hello World <joaop.codes@gmail.com>
2024-08-17 21:34:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function text1() {
     titulo.innerHTML = "Hello World 👋🏼"
     h4.innerHTML = "Sou João Piornedo"
     text.innerHTML = "Sou um desenvolvedor web com experiência em projetos de desenvolvimento de sites totalmente responsivos, utilizando tecnologias modernas para garantir a melhor experiência do usuário. Estou constantemente aprendendo e me atualizando sobre novas linguagens e frameworks. Tenho um forte foco em práticas de design responsivo, acessibilidade e otimização de performance, assegurando que os sites funcionem perfeitamente em diversos dispositivos e navegadores."
-    ft.style.top = '20em'; 
+    ft.style.top = '20em';
     ft.style.left = '9em';
 }
 
@@ -69,7 +69,7 @@ function text2() {
     titulo.innerHTML = "Meus Objetivos"
     h4.innerHTML = ""
     text.innerHTML = "Meu objetivo é me juntar a uma grande empresa onde eu possa me especializar e crescer profissionalmente. Com foco, determinação e uma facilidade notável para aprender, estou comprometido em me tornar um especialista em minha área. Acredito que trabalhar em um ambiente desafiador e inovador me permitirá aprimorar minhas habilidades e contribuir significativamente para projetos de alto impacto."
-    ft.style.top = '20em'; 
+    ft.style.top = '20em';
     ft.style.left = '9em';
 }
 
@@ -78,7 +78,7 @@ function text3() {
     titulo.innerHTML = "Áreas de Estudo"
     h4.innerHTML = ""
     text.innerHTML = "Atualmente, estou estudando tecnologias como React, Node.js e JavaScript avançado. Estou sempre em busca de aprender e dominar novas tecnologias, mantendo-me atualizado e aprimorando minhas habilidades continuamente. Acredito que o aprendizado constante é fundamental para o crescimento profissional e para estar à frente no mercado de tecnologia."
-    ft.style.top = '20em'; 
+    ft.style.top = '20em';
     ft.style.left = '9em';
 }
 
@@ -95,30 +95,30 @@ var p3 = document.querySelector('.p3')
 var h1_proj = p3.querySelector('h1')
 var p_proj = p3.querySelector('p')
 
-function p1_selecao(){
-    h1_proj.innerHTML='Page Animals '
-    p_proj.innerHTML='Este projeto consiste em uma página web interativa, focada em artigos sobre animais. O design incorpora um layout dinâmico e cores vibrantes, criando uma experiência visual cativante para o usuário. O site é 100% responsivo, garantindo uma navegação fluida em qualquer dispositivo. Foram testadas diversas paletas de cores para atrair e envolver os usuários, além de animações sutis que melhoram a fluidez do site e a experiência de navegação.'
+function p1_selecao() {
+    h1_proj.innerHTML = 'Page Animals '
+    p_proj.innerHTML = 'Este projeto consiste em uma página web interativa, focada em artigos sobre animais. O design incorpora um layout dinâmico e cores vibrantes, criando uma experiência visual cativante para o usuário. O site é 100% responsivo, garantindo uma navegação fluida em qualquer dispositivo. Foram testadas diversas paletas de cores para atrair e envolver os usuários, além de animações sutis que melhoram a fluidez do site e a experiência de navegação.'
 }
-function p1_out(){
-   h1_proj.innerHTML='Descrição'
-   p_proj.innerHTML= ''
+function p1_out() {
+    h1_proj.innerHTML = 'Descrição'
+    p_proj.innerHTML = ''
 }
-p1.addEventListener('mouseover',p1_selecao)
-p1.addEventListener('mouseout',p1_out)
+p1.addEventListener('mouseover', p1_selecao)
+p1.addEventListener('mouseout', p1_out)
 
 //P2
 var p2 = document.querySelector('.p2')
 
-function p2_selecao(){
-    h1_proj.innerHTML='Page Poema '
-    p_proj.innerHTML='Este projeto foi desenvolvido com o objetivo de aplicar o efeito parallax em uma página web de layout responsivo. O conteúdo principal é um poema, complementado por animações de scroll que aprimoram a interatividade do usuário. O site foi inicialmente criado como parte de um exercício de um curso que fiz, mas foi adaptado e aprimorado com melhorias significativas.'
+function p2_selecao() {
+    h1_proj.innerHTML = 'Page Poema '
+    p_proj.innerHTML = 'Este projeto foi desenvolvido com o objetivo de aplicar o efeito parallax em uma página web de layout responsivo. O conteúdo principal é um poema, complementado por animações de scroll que aprimoram a interatividade do usuário. O site foi inicialmente criado como parte de um exercício de um curso que fiz, mas foi adaptado e aprimorado com melhorias significativas.'
 }
-function p2_out(){
-   h1_proj.innerHTML='Descrição'
-   p_proj.innerHTML= ''
+function p2_out() {
+    h1_proj.innerHTML = 'Descrição'
+    p_proj.innerHTML = ''
 }
-p2.addEventListener('mouseover',p2_selecao)
-p2.addEventListener('mouseout',p2_out)
+p2.addEventListener('mouseover', p2_selecao)
+p2.addEventListener('mouseout', p2_out)
 
 // Redes Sociais:
 
